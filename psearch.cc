749d4fc27ff99d246e8e2c7b43de7b1f82ab0b04
SharapovDenis <sharapov.d@phystech.edu>
2021-11-20 15:22:09 +0300

    version 1.0.2

--- psearch.cc
+++ psearch.cc
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <sys/types.h>
-#include <dirent.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
